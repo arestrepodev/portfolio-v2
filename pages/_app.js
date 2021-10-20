b6115d907f7805e05c50a115ae3c9eff3695713f
Arnold Restrepo Hernandez <arnold.restrepohernandez@gmail.com>
2021-10-20 01:24:44 -0500

    Add Custom Fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main'
 import theme from '../lib/theme'
+import Fonts from '../components/fonts'
 const Website = ({ Component, pageProps, router }) => {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
