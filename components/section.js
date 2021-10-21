316a059518461885ee098282a2959da243269919
Arnold Restrepo Hernandez <arnold.restrepohernandez@gmail.com>
2021-10-21 01:00:27 -0500

    Section and Header

---
+++ components/section.js
@@ -0,0 +1,23 @@
+import { motion } from 'framer-motion'
+import { chakra, shouldForwardProp } from '@chakra-ui/react'
+
+const StyleDiv = chakra(motion.div, {
+  shouldForwardProp: prop => {
+    return shouldForwardProp(prop) || prop === 'transition'
+  }
+})
+
+const Section = ({ children, delay = 0 }) => {
+  return (
+    <StyleDiv
+      initial={{ y: 0, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.8, delay }}
+      mb={6}
+    >
+      {children}
+    </StyleDiv>
+  )
+}
+
+export default Section
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,64 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react'
+import Section from '../components/Section'
 
 const Page = () => {
   return (
-    <Container>
+    <Container maxW="container.xl">
       <Box
         borderRadius="lg"
-        backgroundColor="darkcyan"
+        bg={useColorModeValue('darkcyan', 'whiteAlpha.200')}
         color="white"
         p={3}
         mb={6}
+        mt={6}
         align="center"
       >
-        hello I'am a software frontend developer based in Colombia
+        Hello I'am a software frontend developer based in Colombia
       </Box>
-      <Box as="section" display={{ md: 'flex' }}>
-        <Box flexFlow={1}>
+      <Box
+        as="section"
+        display={{ md: 'flex' }}
+        justifyContent={{ md: 'space-between' }}
+        alignItems="center"
+      >
+        <Box flexFlow={1} textAlign={{ xs: 'center' }}>
           <Heading as="h2" variant="page-title">
             Arnold Restrepo Hernandez
           </Heading>
-          <p>Digital Craftzman (Musician / Developer)</p>
+          <Text as="p" fontSize="sm">
+            Frontend Developer / Musician
+          </Text>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.500"
+            borderWidth={3}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/profile-arnold-restrepo.jpeg"
+          />
         </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Skills
+        </Heading>
+        <p>Paragraph</p>
+      </Section>
     </Container>
   )
 }
