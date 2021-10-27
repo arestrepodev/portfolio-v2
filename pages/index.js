22de258f644ef4fc844983289fabbd0dc27d29c7
Arnold Restrepo Hernandez <arnold.restrepohernandez@gmail.com>
2021-10-27 01:22:02 -0500

    Add layout animation

--- pages/index.js
+++ pages/index.js
@@ -16,94 +16,97 @@ import Paragraph from '../components/paragraph'
 import { Section } from '../components/section'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
 
 const Page = () => {
   const { t } = useTranslation()
   return (
-    <Container maxW="container.xl">
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('darkcyan', 'whiteAlpha.200')}
-        color="white"
-        p={3}
-        mb={6}
-        mt={6}
-        align="center"
-      >
-        {t('common.intro')}
-      </Box>
-      <Box
-        as="section"
-        display={{ md: 'flex' }}
-        justifyContent={{ md: 'space-between' }}
-        alignItems="center"
-      >
-        <Box flexFlow={1} textAlign={{ xs: 'center' }}>
-          <Heading as="h2" variant="page-title">
-            Arnold Restrepo Hernandez
-          </Heading>
-          <Text as="p" fontSize="sm">
-            Frontend Developer / Musician
-          </Text>
-        </Box>
+    <Layout>
+      <Container maxW="container.xl">
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          bg={useColorModeValue('darkcyan', 'whiteAlpha.200')}
+          color="white"
+          p={3}
+          mb={6}
+          mt={6}
           align="center"
         >
-          <Image
-            borderColor="whiteAlpha.500"
-            borderWidth={3}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/profile-arnold-restrepo.jpeg"
-          />
+          {t('common.intro')}
         </Box>
-      </Box>
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          {t('common.about-title')}
-        </Heading>
-        <Paragraph indent={true}>{t('about-me.intro')}</Paragraph>
-        <Paragraph>{t('about-me.passionate')}</Paragraph>
-        <Paragraph>{t('about-me.current-job')}</Paragraph>
-        <Box align="left" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon />}>
-              {t('common.portfolio-button')}
-            </Button>
-          </NextLink>
+        <Box
+          as="section"
+          display={{ md: 'flex' }}
+          justifyContent={{ md: 'space-between' }}
+          alignItems="center"
+        >
+          <Box flexFlow={1} textAlign={{ xs: 'center' }}>
+            <Heading as="h2" variant="page-title">
+              Arnold Restrepo Hernandez
+            </Heading>
+            <Text as="p" fontSize="sm">
+              Frontend Developer / Musician
+            </Text>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              borderColor="whiteAlpha.500"
+              borderWidth={3}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/profile-arnold-restrepo.jpeg"
+            />
+          </Box>
         </Box>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          {t('common.bio-title')}
-        </Heading>
-        <BioSection>
-          <BioYear>1997</BioYear>
-          {t('bio.born')}
-        </BioSection>
-        <BioSection>
-          <BioYear>2016</BioYear>
-          {t('bio.career')}
-        </BioSection>
-        <BioSection>
-          <BioYear>2017</BioYear>
-          {t('bio.ondigital-job')}
-        </BioSection>
-        <BioSection>
-          <BioYear>2019</BioYear>
-          {t('bio.quick-job')}
-        </BioSection>
-        <BioSection>
-          <BioYear>2021 {t('bio.current-title')}</BioYear>
-          {t('bio.addi-job')}
-        </BioSection>
-      </Section>
-    </Container>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            {t('common.about-title')}
+          </Heading>
+          <Paragraph indent={true}>{t('about-me.intro')}</Paragraph>
+          <Paragraph>{t('about-me.passionate')}</Paragraph>
+          <Paragraph>{t('about-me.current-job')}</Paragraph>
+          <Box align="left" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />}>
+                {t('common.portfolio-button')}
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            {t('common.bio-title')}
+          </Heading>
+          <BioSection>
+            <BioYear>1997</BioYear>
+            {t('bio.born')}
+          </BioSection>
+          <BioSection>
+            <BioYear>2016</BioYear>
+            {t('bio.career')}
+          </BioSection>
+          <BioSection>
+            <BioYear>2017</BioYear>
+            {t('bio.ondigital-job')}
+          </BioSection>
+          <BioSection>
+            <BioYear>2019</BioYear>
+            {t('bio.quick-job')}
+          </BioSection>
+          <BioSection>
+            <BioYear>2021 {t('bio.current-title')}</BioYear>
+            {t('bio.addi-job')}
+          </BioSection>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -3,33 +3,43 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { SimpleGrid, Divider, Container, Heading, Box } from '@chakra-ui/react'
 import { Section } from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
+// Note: Import Portfolio images here
 import petgram from '../public/images/works/petgram.jpeg'
 import servientregaInternational from '../public/images/works/servientrega-international-portafolio.jpeg'
 const Works = () => {
   return (
-    <Container maxW="container.xl">
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <WorkGridItem id={1} title="Petgram - PetLife" thumbnail={petgram}>
-            La red social favorita de nuestras mascotas. Hecha con React.js y
-            GraphQL.
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem
-            id={1}
-            title="Servientrega International"
-            thumbnail={servientregaInternational}
-          >
-            Rediseñar la UX y mejorar la Arquitectura de Información fueron los
-            objetivos para mejorar la web de Servientrega.
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+    <Layout>
+      <Container maxW="container.xl">
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
+        <Box mb={6}>
+          <p>
+            A litle compilation of some importants, challenging, personals and
+            professionals works, which I've made in my career.
+          </p>
+        </Box>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem id={1} title="Petgram - PetLife" thumbnail={petgram}>
+              La red social favorita de nuestras mascotas. Hecha con React.js y
+              GraphQL.
+            </WorkGridItem>
+          </Section>
+          <Section>
+            <WorkGridItem
+              id={1}
+              title="Servientrega International"
+              thumbnail={servientregaInternational}
+            >
+              Rediseñar la UX y mejorar la Arquitectura de Información fueron
+              los objetivos al hacer este proyecto.
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
